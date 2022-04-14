e7f161600d0455fa0718082c57f0afb45de63c4f
Danil Vakhrushev <danil1209@yandex.ry>
2022-04-14 19:39:58 +0300

    Initial commit

---
+++ src/main/java/com/example/zigbeepolivalka/ZigbeePolivalkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.zigbeepolivalka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZigbeePolivalkaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZigbeePolivalkaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/zigbeepolivalka/ZigbeePolivalkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zigbeepolivalka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZigbeePolivalkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
